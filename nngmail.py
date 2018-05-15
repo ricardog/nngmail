5581a4e50d078409db3b12fe26a73e5cc2ae881f
Ricardo E. Gonzalez <ricardog@ricardog.com>
2018-05-15 11:37:45 +0100

    Added code for incremental updates
    First fetch all new changes, then merge the updates, and then apply the
    updates.  This means that if a message gets added and deleted in the
    same history sequence, then the message never gets added to the local
    database.  Similarly adding a removing a label (both of which happen
    within this incremental update) will never show up in the local
    database.

--- nngmail.py
+++ nngmail.py
@@ -40,8 +40,6 @@ class NnGmail():
             results = self.gmail.get_messages(gids, 'metadata')
             for batch in results:
                 for msg in batch:
-                    if msg['id'] in('15c677afd6ef4607', '1574e5d1d2c37ffa'):
-                        pdb.set_trace()
                     self.sql3.store(msg['id'], msg['threadId'],
                                     msg.get('labelIds', []),
                                     int(msg['internalDate']) / 1000,
@@ -71,32 +69,82 @@ class NnGmail():
             self.sql3.session.flush()
         return history_id
 
+    def update2(self, updated):
+        for id in updated.keys():
+            self.sql3.update(id, updated[id])
+        self.sql3.session.flush()
+
     def delete(self, gids):
         self.sql3.delete(gids)
 
+    def merge_history(self, history):
+        deleted = {}
+        added = {}
+        updated = {}
+        
+        bar = tqdm(leave=True, total=len(history) - 1, desc='merging changes')
+        for item in map(lambda i: history[i],
+                        range(len(history) - 1, 0, -1)):
+            if 'messagesDeleted' in item:
+                for msg in map(lambda m: m['message'],
+                               item['messagesDeleted']):
+                    deleted[msg['id']] = True
+            if 'messagesAdded' in item:
+                for msg in filter(lambda m: m['id'] not in deleted,
+                                  map(lambda m: m['message'],
+                                      item['messagesAdded'])):
+                    added[msg['id']] = msg
+            for kind in ('labelsAdded', 'labelsRemoved'):
+                if kind in item:
+                    for msg in filter(lambda m: (m['id'] not in deleted and
+                                                 m['id'] not in added),
+                                      map(lambda m: m['message'],
+                                          item[kind])):
+                        if msg['id'] not in updated:
+                            updated[msg['id']] = msg['labelIds']
+            bar.update()
+        bar.close()
+        return(int(history[-1]['id']), deleted, added, updated)
+
     def pull(self):
-        total = 1
-        bar = tqdm(leave=True, total=total, desc='fetching changes')
+        need_full = False
         history_id = self.sql3.get_history_id()
         self.sync_labels()
-
+        history = []
+        print('history id: %d' % history_id)
+        
+        bar = tqdm(leave=True, total=10, desc='fetching changes')
         try:
             for results in self.gmail.get_history_since(history_id):
-                pdb.set_trace()
-                continue
-                (total, msgs) = results
-                gids = set([msg['id'] for msg in msgs])
-                bar.total = total
-                hid1 = self.store(gids - local_gids)
-                hid2 = self.update(local_gids.intersection(gids))
-                history_id = max(hid1, hid2, history_id)
-                local_gids = local_gids - gids
-                bar.update(len(msgs))
-            self.sql3.set_history_id(history_id)
+                history += results
+                bar.update(1)
         except remote.Gmail.NoHistoryException as ex:
-            bar.close()
+            need_full = True
+        bar.close()
+
+        if need_full:
             print('No history available; attempting full pull')
             self.full_pull()
+            return
+
+        if len(history) == 0:
+            print('no new changes')
+            return
+
+        hid, deleted, added, updated = self.merge_history(history)
+        assert hid >= history_id
+        total = len(deleted) + len(added) + len(updated)
+        bar = tqdm(leave=True, total=total, desc='applying changes')
+        self.delete(tuple(deleted.keys()))
+        bar.update(len(deleted))
+        new_gids = set(added.keys())
+        local_gids = set(map(lambda mm: mm.google_id,
+                             self.sql3.find_by_gid(new_gids)))
+        _ = self.store(new_gids - local_gids)
+        bar.update(len(added))
+        self.update2(updated)
+        bar.close()
+        self.sql3.set_history_id(hid)
                   
     def full_pull(self):
         total = 1
@@ -118,6 +166,7 @@ class NnGmail():
         for gid in local_gids:
             self.delete(gid)
         bar.close()
+        print('new historyId: %d' % history_id)
         self.sql3.set_history_id(history_id)
         
 def main():
