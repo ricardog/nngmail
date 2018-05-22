fba2c2e7465071859438beb5a94f4df026129023
Ricardo E. Gonzalez <ricardog@ricardog.com>
2018-05-22 19:48:22 +0100

    Start working on the gnus side of things
    This is mostly copy-and-rename from nnimap.el (the early stuff).

---
+++ nngmail.el
@@ -0,0 +1,45 @@
+(eval-and-compile
+  (require 'nnheader)
+  ;; In Emacs 24, `open-protocol-stream' is an autoloaded alias for
+  ;; `make-network-stream'.
+  (unless (fboundp 'open-protocol-stream)
+    (require 'proto-stream)))
+
+(eval-when-compile
+  (require 'cl))
+
+(require 'nnheader)
+(require 'gnus-util)
+(require 'gnus)
+(require 'nnoo)
+(require 'netrc)
+(require 'utf7)
+(require 'tls)
+(require 'parse-time)
+(require 'nnmail)
+
+(nnoo-declare nngmail)
+
+(defvoo nngmail-address "localhost"
+  "The address of the gmail proxy.")
+
+(defvoo nngmail-user nil
+  "Username to use for authentication to the IMAP server.")
+
+(defvoo nngmail-server-port 1234
+  "The port the gmail proxy listens on.")
+
+(defvoo nngmail-split-methods nil
+  "How mail is split.
+Uses the same syntax as `nnmail-split-methods'.")
+
+(defvoo nngmail-split-fancy nil
+  "Uses the same syntax as `nnmail-split-fancy'.")
+
+(defun nngmail-decode-gnus-group (group)
+  (decode-coding-string group 'utf-8))
+
+(defun nngmail-encode-gnus-group (group)
+  (encode-coding-string group 'utf-8))
+
+(defvar nngmail-status-string "")
