e34dc973467c486d844189f3ed334039fb4e9d62
Ricardo E. Gonzalez <ricardog@ricardog.com>
2018-06-02 22:28:55 +0100

    Convert account to use MethodView
    Which seems to make things a bit cleaner and tidier.

--- nngmail/views.py
+++ nngmail/views.py
@@ -1,5 +1,6 @@
 import click
 from flask import jsonify, make_response, request
+from flask.views import MethodView
 
 from nngmail import app, db
 from nngmail.models import *
@@ -8,39 +9,50 @@ from nngmail.models import *
 def index():
     return 'Hello World!'
 
-@app.route('/api/v1.0/accounts', methods=['GET'])
-def get_accounts():
-    return jsonify({'accounts': Account.serialize_list(Account.query.all())})
+class AccountAPI(MethodView):
+    def get(self, account_id):
+        if not account_id:
+            ## Return list
+            return jsonify({'accounts':
+                            Account.serialize_list(Account.query.all())})
+        else:
+            ## Return single
+            account = Account.query.get(account_id)
+            if not account:
+                return make_response(jsonify({'error': 'Not found'}), 404)
+            return jsonify(account.serialize())
 
-@app.route('/api/v1.0/accounts/<int:account_id>', methods=['GET'])
-def get_account(account_id):
-    account = Account.query.get(account_id)
-    if not account:
-        return make_response(jsonify({'error': 'Not found'}), 404)
-    return jsonify(account.serialize())
+    def post(self):
+        abort(404)
 
-@app.route('/api/v1.0/accounts/<int:account_id>', methods=['PUT'])
-def update_account(account_id):
-    account = Account.query.get(account_id)
-    if not account:
-        return make_response(jsonify({'error': 'Not found'}), 404)
-    if not request.json:
-        return make_response(jsonify({'error': 'No data'}), 400)
-    if ('nickname' not in request.json or
-        not isinstance(request.json['nickname'], str)):
-        return make_response(jsonify({'error': 'Bad nickname'}), 400)
-    account.nickname = request.json['nickname']
-    db.session().commit()
-    return jsonify(account.serialize())
+    def delete(self, user_id):
+        account = Account.query.get(account_id)
+        if not account:
+            return make_response(jsonify({'error': 'Not found'}), 404)
+        db.session().delete(account)
+        db.session().commit()
+        return jsonify({'result': True})
 
-@app.route('/api/v1.0/accounts/<int:account_id>', methods=['DELETE'])
-def delete_account(account_id):
-    account = Account.query.get(account_id)
-    if not account:
-        return make_response(jsonify({'error': 'Not found'}), 404)
-    db.session().delete(account)
-    db.session().commit()
-    return jsonify({'result': True})
+    def put(self, account_id):
+        account = Account.query.get(account_id)
+        if not account:
+            return make_response(jsonify({'error': 'Not found'}), 404)
+        if not request.json:
+            return make_response(jsonify({'error': 'No data'}), 400)
+        if ('nickname' not in request.json or
+            not isinstance(request.json['nickname'], str)):
+            return make_response(jsonify({'error': 'Bad nickname'}), 400)
+        account.nickname = request.json['nickname']
+        db.session().commit()
+        return jsonify(account.serialize())
+
+account_view = AccountAPI.as_view('account_api')
+app.add_url_rule('/api/v1.0/accounts/', defaults={'account_id': None},
+                 view_func=account_view, methods=['GET',])
+app.add_url_rule('/api/v1.0/accounts/', view_func=account_view,
+                 methods=['POST',])
+app.add_url_rule('/api/v1.0/accounts/<int:account_id>', view_func=account_view,
+                 methods=['GET', 'PUT', 'DELETE'])
 
 @app.route('/api/v1.0/contacts', methods=['GET'])
 def contacts():
