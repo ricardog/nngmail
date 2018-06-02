c3186cc9015119cd277db324f5477d011bd9da97
Ricardo E. Gonzalez <ricardog@ricardog.com>
2018-06-02 21:31:27 +0100

    Implemented CRUD methods for account

--- nngmail/views.py
+++ nngmail/views.py
@@ -1,5 +1,5 @@
 import click
-from flask import jsonify
+from flask import jsonify, make_response, request
 
 from nngmail import app, db
 from nngmail.models import *
@@ -8,9 +8,39 @@ from nngmail.models import *
 def index():
     return 'Hello World!'
 
-@app.route('/api/v1.0/accounts', methods=['GET', 'POST', 'DELETE'])
-def accounts():
+@app.route('/api/v1.0/accounts', methods=['GET'])
+def get_accounts():
     return jsonify({'accounts': Account.serialize_list(Account.query.all())})
+
+@app.route('/api/v1.0/accounts/<int:account_id>', methods=['GET'])
+def get_account(account_id):
+    account = Account.query.get(account_id)
+    if not account:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+    return jsonify(account.serialize())
+
+@app.route('/api/v1.0/accounts/<int:account_id>', methods=['PUT'])
+def update_account(account_id):
+    account = Account.query.get(account_id)
+    if not account:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+    if not request.json:
+        return make_response(jsonify({'error': 'No data'}), 400)
+    if ('nickname' not in request.json or
+        not isinstance(request.json['nickname'], str)):
+        return make_response(jsonify({'error': 'Bad nickname'}), 400)
+    account.nickname = request.json['nickname']
+    db.session().commit()
+    return jsonify(account.serialize())
+
+@app.route('/api/v1.0/accounts/<int:account_id>', methods=['DELETE'])
+def delete_account(account_id):
+    account = Account.query.get(account_id)
+    if not account:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+    db.session().delete(account)
+    db.session().commit()
+    return jsonify({'result': True})
 
 @app.route('/api/v1.0/contacts', methods=['GET'])
 def contacts():
