01c7020c5e174d6d45926a4345a7aa6e196210a6
Ricardo E. Gonzalez <ricardog@ricardog.com>
2018-06-03 14:07:44 +0100

    Remove account_required crap
    This gets handled automatically by the router.

--- nngmail/views/message.py
+++ nngmail/views/message.py
@@ -6,17 +6,7 @@ from flask.views import MethodView
 from nngmail import db
 from nngmail.models import Account, Message
 
-def account_required(f):
-    """Checks whether an account was given or raises error 401."""
-    def decorator(*args, **kwargs):
-        if 'account_id' not in request.json:
-            make_response(jsonify({'error': 'Specify account'}), 401)
-        return f(*args, **kwargs)
-    return decorator
-
 class MessageAPI(MethodView):
-    #decorators = [account_required]
-    
     def get(self, account_id, message_id):
         if not message_id:
             ## Return list
