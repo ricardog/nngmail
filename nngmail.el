56cecd5cf736630317f7e396918f033339204df6
Ricardo E. Gonzalez <ricardog@ricardog.com>
2018-06-03 21:43:41 +0100

    Created a template of gnus back end API
    Most functions (either required or optional) are just stubs.  But this
    gives me a good idea of all the functions I need to implement.

--- nngmail.el
+++ nngmail.el
@@ -40,11 +40,15 @@ Uses the same syntax as `nnmail-split-methods'.")
 (defvoo nngmail-split-fancy nil
   "Uses the same syntax as `nnmail-split-fancy'.")
 
+(defvoo nngmail-header-format "nov")
+
 (defun nngmail-decode-gnus-group (group)
   (decode-coding-string group 'utf-8))
 
 (defun nngmail-encode-gnus-group (group)
   (encode-coding-string group 'utf-8))
+
+(defvar nngmail-last-account-id nil)
 
 (defvar nngmail-status-string "")
 
@@ -59,7 +63,7 @@ Uses the same syntax as `nnmail-split-methods'.")
 	 (proc (start-process "nngmail" buf program config-file)))
     (message "nngmail sync daemon started")))
 
-(defvar nngmail-virtual-servers ())
+(defvar nngmail-servers ())
 
 (defun nngmail-get-account-params (elem)
   (let ((nickname (plist-get elem 'nickname))
@@ -68,7 +72,7 @@ Uses the same syntax as `nnmail-split-methods'.")
     (cons nickname (list id email))))
 
 (defun nngmail-get-account (nickname)
-  (cdr (assoc-string nickname nngmail-virtual-servers)))
+  (cdr (assoc-string nickname nngmail-servers)))
 
 (defun nngmail-get-account-email (nickname)
   (elt (nngmail-get-account nickname) 1))
@@ -77,8 +81,6 @@ Uses the same syntax as `nnmail-split-methods'.")
   (elt (nngmail-get-account nickname) 0))
 
 (defun nngmail-parse-accounts ()
-  (setq nngmail-virtual-servers ())
-  (goto-char url-http-end-of-headers)
   (let ((json-object-type 'plist)
         (json-key-type 'symbol)
         (json-array-type 'vector))
@@ -88,7 +90,7 @@ Uses the same syntax as `nnmail-split-methods'.")
       ;; Do something with RESULT here
       (seq-map
        (lambda (elem)
-	 (push (nngmail-get-account-params elem) nngmail-virtual-servers))
+	 (push (nngmail-get-account-params elem) nngmail-servers))
        accounts)
       )))
 
@@ -97,25 +99,144 @@ Uses the same syntax as `nnmail-split-methods'.")
   (format "%s/%ss/" nngmail-base-url resource))
 
 (defun nngmail-get-accounts ()
-  (url-retrieve (nngmail-url-for "account")
-		(lambda (events)
-		  (nngmail-parse-accounts)))
-  (message (format "%s: %s"
-		   (nngmail-get-account-id "itineris")
-		   (nngmail-get-account-email "itineris"))))
+  (let ((buffer (url-retrieve-synchronously (nngmail-url-for "account") t)))
+    (setq nngmail-servers ())
+    (with-current-buffer buffer
+      (goto-char url-http-end-of-headers)
+      (nngmail-parse-accounts buffer))))
 
-(deffoo nngmail-open-server (nickname email)
+(defun nngmail-touch-server (server)
+  (setq nngmail-last-account-id (nngmail-get-account server)))
+	
+(deffoo nngmail-open-server (server &rest definitions)
   "Verify the nngmail server syncs the account."
   (interactive)
   (nngmail-get-accounts)
-  (unless (nngmail-get-account nickname)
+  (unless (nngmail-get-account server)
     (error
-     (format "You are not synching the account %s:%s" email nickname)))
-  (unless
-      (string-equal (nngmail-get-account-email nickname) email)
-    (error (format "Email address mismatch %s != %s" email
-		   (nngmail-get-account-email nickname)))))
-			
+     (format "You are not synching the account %s" server)))
+  (and
+   (assoc-string "email" definitions)
+   (let ((email-def (assoc-string "email" definitions))
+	 (email-ser (nngmail-get-account-email server)))
+     (unless
+       (string-equal email-ser email-def)
+       (error (format "Email address mismatch %s != %s"
+		   email-ser email-def)))))
+  (message (format "nngmail: opened server '%s'" server))
+  (nngmail-touch-server server))
+
+(deffoo nngmail-close-server (server)
+  "Close connection to server.  Removes the server from the
+accounts alist."
+  (setq nngmail-servers
+	(delq (assoc-string server nngmail-servers) nngmail-servers))
+  (message (format "nngmail: closed server '%s'" server))
+  (and (eq nngmail-last-account-id
+	   (nngmail-get-account-id server))
+       (setq nngmail-last-acount-id nil)))
+
+(deffoo nngmail-request-close ()
+  "Close connection to all servers.  Removes all entries from the
+accounts alist."
+  (setq nngmail-servers ()
+	nngmail-last-account-id nil)
+  (and
+   (message (format "nngmail: closed server '%s'" server))
+   nil))
+
+(deffoo nngmail-server-opened (&optional server)
+  "Returns whether the server exists in the accounts alist"
+  (nngmail-get-account-id server))
+
+(deffoo nngmail-retrieve-headers (articles &optional group server fetch-old)
+  "Retrieve headers for the specified group (label)."
+  nil
+  )
+
+(deffoo nngmail-status-message (&optional server)
+  nngmail-status-string)
+
+(deffoo nngmail-request-article (article &optional group server to-buffer)
+  "Issue an HTTP request for the raw article body."
+  (and
+   (cons group article)
+   nil))
+
+(deffoo nngmail-request-group (group &optional server fast info)
+  "Retrieve information about GROUP."
+  ;;; 211 56 1000 1059 ifi.discussion
+  nil)
+  
+(deffoo nngmail-close-group (group &optional server)
+  "Close the group.  A nop."
+  nil)
+
+(deffoo nngmail-request-list (&optional server)
+  "Return a list of all groups available on SERVER."
+  ;;; An example of a server with two groups
+  ;;;
+  ;;; ifi.test 0000002200 0000002000 y
+  ;;; ifi.discussion 3324 3300 n
+  nil)
+
+(deffoo nngmail-request-post (&optional server)
+  (setq nngmail-status-string "Read-only server")
+  nil)
+
+(deffoo nngmail-request-type (group &optional article)
+  'mail)
+
+(defvar nngmail-mark-alist
+  '((read "UNREAD")
+    (tick "FLAGGED")
+    (reply "Answered")
+    (expire "expired")
+    (dormant "dormant")
+    (score "score")
+    (save "save")
+    (download "download")
+    (forward "forward")))
+
+(deffoo nngmail-request-set-mark (group action &optional server)
+  "Set/remove/add marks on articles."
+  ;;; ACTION is a list of mark setting requests, having this format:
+  ;;; (RANGE ACTION MARK)
+  ;;; Gnus marks are:
+
+  nil)
+
+(deffoo nngmail-request-scan (&optional group server)
+  "Check for new articles.  If possible only on GROUP (although
+that makes no sense for this backend since new (unread) mail will
+appear in INBOX."
+  nil)
+
+(deffoo nngmail-request-newsgroups (date &optional server)
+  "Returns a list of newsgroups created after DATE."
+  nil)
+
+(deffoo nngmail-request-expire-articles (articles &optional group server force)
+  "Expire articles."
+  nil)
+
+(deffoo nngmail-request-move-article (article group server accept-form
+					      &optional last)
+  "Move article to a new group."
+  nil)
+
+(deffoo nngmail-request-accept-article (group &optional server last)
+  "Used for respooling?"
+  nil)
+
+(deffoo nngmail-request-delete-group (group force &optional server)
+  ""
+  nil)
+
+(deffoo nngmail-request-rename-group (group new-name &optional server)
+  ""
+  nil)
+
 
 (provide 'nngmail)
 ;;; nngmail.el ends here
