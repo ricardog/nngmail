9610ecd5667f3248ac4c2f26273987ec0c424603
Ricardo E. Gonzalez <ricardog@ricardog.com>
2018-06-17 23:31:03 +0100

    Query needs to return message ID's not google ID's
    So do a local SQL query to get the ID's.  In addition, filter out
    messages so each message appears only once.  Although I am unclear
    whether this matter (and whether my solution here is correct).

--- nngmail/views/query.py
+++ nngmail/views/query.py
@@ -1,24 +1,32 @@
 from flask import jsonify, request
 from flask.views import MethodView
+import urllib
 
 from nngmail import app, db, get_sync
-from nngmail.models import Account
+from nngmail.models import Account, label_association, Label, Message
 from nngmail.views.utils import acct_base, acct_nick_base
 
 class QueryAPI(MethodView):
     def get(self, account_id):
-        labels = request.args.get('labels', '').split(',')
+        label_arg = request.args.get('labels', '')
+        labels = urllib.parse.unquote(label_arg).split(',')
         query = request.args.get('q', '')
         gmail = get_sync(Account.query.get(account_id))
-        result = gmail.search(query, labels)
+        gids = gmail.search(query, labels)
+        # Convert Google ID's to message ID's.
+        result = Message.query.filter_by(account_id=account_id).\
+            join(label_association).join(Label).\
+            with_entities(Message.id, Label.name).\
+            filter(Message.google_id.in_(gids)).\
+            filter(Label.name.in_(labels)).order_by(Message.id.desc()).all()
         return jsonify({'result': result})
 
 ## Query resource
 query_view = QueryAPI.as_view('query_api')
-app.add_url_rule(acct_base + '/query/', view_func=query_view,
+app.add_url_rule(acct_base + '/querys/', view_func=query_view,
                  methods=['GET'])
 
-@app.route(acct_nick_base + '/query/', methods=['GET'])
+@app.route(acct_nick_base + '/querys/', methods=['GET'])
 def query_with_nick(nickname):
     return query_view(Account.query.filter_by(nickname=nickname).\
                       first_or_404().id)
