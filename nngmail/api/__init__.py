b1b0f808e06e836753711bc73cf2c2a8a3f1c757
Ricardo E. Gonzalez <ricardog@ricardog.com>
2018-06-18 15:52:03 +0100

    Fixed bogus contact route

--- nngmail/api/__init__.py
+++ nngmail/api/__init__.py
@@ -1,5 +1,5 @@
 
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 api_bp = Blueprint('api', __name__, url_prefix='/api/v1.0',
                    template_folder='templates')
@@ -10,7 +10,7 @@ import nngmail.api.message
 import nngmail.api.label
 import nngmail.api.query
 import nngmail.api.thread
-
+from nngmail.models import Contact
 
 @api_bp.route('/contacts', methods=['GET'])
 def contacts():
