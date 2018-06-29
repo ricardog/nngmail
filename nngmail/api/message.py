c00c4d4536cace212baf39a5073fc0f2502aca48
Ricardo E. Gonzalez <ricardog@ricardog.com>
2018-06-29 14:00:37 +0100

    Simplify the query logic in message and cleanup
    Avoid going to the database twice per request.
    
    Removed unused code (no longer use q parameter for query).

--- nngmail/api/message.py
+++ nngmail/api/message.py
@@ -5,43 +5,44 @@ from flask import abort, jsonify, make_response, render_template, request
 from flask.views import MethodView
 from sqlalchemy.orm import undefer
 
-from nngmail import db, get_sync, zync
+from nngmail import db, get_sync
 from nngmail.api import api_bp
 from nngmail.models import Account, Label, Message
 from nngmail.api.utils import acct_base, acct_nick_base
 
 def to_range(r):
+    """Convert a (low, high) tuple to a sequence of integers."""
     if len(r) == 2:
         return tuple(range(int(r[0]), int(r[1])+1))
     return (int(r[0]), )
 
 def get_ids(arg):
+    """Convert a range string to a sequence of integers.
+
+A range string consists of comman separated ranges.  Each range is
+either a single number, or a low:high tuple.
+
+    """
     return sum(map(lambda r: to_range(r),
                    map(lambda s: s.split(':'),
                        arg.split(','))), ())
-    
+
 class MessageAPI(MethodView):
     def get(self, account_id, message_id):
         if not message_id:
             ## Return list
-            filters = {}
             if 'label' in request.args:
+                ## FIXME: Move this functionality to label controller
                 query = Message.by_label(account_id,
                                          request.args['label']).distinct()
             else:
                 query = Message.query.filter_by(account_id=account_id)
-            if 'q' in request.args:
-                ## FIXME: generalize search query parameters
-                q = urllib.parse.unquote(request.args['q'])
-                filters.update(dict([q.split('=', 1)]))
-                query = query.filter_by(**filters)
             if 'id' in request.args:
                 ids = get_ids(request.args['id'])
                 query = query.filter(Message.id.in_(ids))
-            if 'thread_id' in request.args:
-                query = query.filter(thread_id=request.args('thread_id'))
             query = query.order_by(Message.id.desc())
             if 'id' not in request.args:
+                ## Must appear after the over_by clause
                 query = query.limit(request.args.get('limit', 200))
             messages = query.all()
 
@@ -53,10 +54,7 @@ class MessageAPI(MethodView):
             return jsonify({'messages': messages})
         else:
             ## Return single
-            message = Message.query.get((message_id, account_id))
-            if not message:
-                return make_response(jsonify({'error': 'Message not found'}),
-                                     404)
+            message = Message.query.get_or_404((message_id, account_id))
             fmt = request.args.get('format', 'json')
             if fmt.lower() == 'raw':
                 message = Message.query.options(undefer('_raw')).\
@@ -64,7 +62,7 @@ class MessageAPI(MethodView):
                 # FIXME: use stream_with_context?
                 if message.raw is None:
                     click.echo('fetching message %d' % message.id)
-                    sync = get_sync(message.account).read(message.id)
+                    get_sync(message.account).read(message.id)
                     message = Message.query.get((message_id, account_id))
                 return make_response(message.raw)
             return jsonify(message)
@@ -80,11 +78,9 @@ class MessageAPI(MethodView):
             return message
 
         if not request.json:
-            click.echo('no json')
             return make_response(jsonify({'error':
                                           'No data for message update'}),
                                  400)
-        click.echo(request.json)
         if ('add_labels' not in request.json and
             'rm_labels' not in request.json):
             return make_response(jsonify({'error':
@@ -131,7 +127,6 @@ class MessageAPI(MethodView):
         db.session().commit()
         return jsonify({'result': True})
 
-
 ## Message resource
 message_view = MessageAPI.as_view('message')
 api_bp.add_url_rule(acct_base + '/messages/', defaults={'message_id': None},
@@ -148,13 +143,14 @@ def messages(nickname):
 @api_bp.route(acct_nick_base + '/messages/<int:message_id>',
               methods=['GET', 'PUT', 'DELETE'])
 def message_by_id(nickname, message_id):
-    message = Message.query.get_or_404((message_id, account.id))
+    message = Message.query.filter(Account.nickname == nickname).\
+        filter_by(id=message_id).first_or_404()
     return message_view(message.account_id, message.id)
 
 @api_bp.route(acct_nick_base + '/messages/<string:message_id>',
-              methods=['GET', 'PUT', 'DELETE'])
+              methods=['GET'])
 def message_by_message_id(nickname, message_id):
     mid = urllib.parse.unquote(message_id)
-    message = Message.query.filter_by(account_id=account.id).\
+    message = Message.query.filter(Account.nickname == nickname).\
         filter_by(message_id=mid).first_or_404()
-    return message_view(messge.account_id, message.id)
+    return message_view(message.account_id, message.id)
