f678720f34fe5d58d53cddff7aea82ba03b9c08c
Ricardo E. Gonzalez <ricardog@ricardog.com>
2018-06-29 12:35:45 +0100

    Cleanup thread view controller

--- nngmail/api/thread.py
+++ nngmail/api/thread.py
@@ -1,5 +1,3 @@
-import click
-
 from flask import jsonify, make_response, render_template, request
 from flask.views import MethodView
 
@@ -27,7 +25,7 @@ class ThreadAPI(MethodView):
                 return render_template('nov.txt', messages=thread.messages)
             return jsonify(thread)
 
-    def delete(self, account_id, thread_id):
+    def delete(self, thread_id):
         thread = Thread.query.get(thread_id)
         if not thread:
             return make_response(jsonify({'error': 'Thread not found'}), 404)
@@ -40,14 +38,13 @@ api_bp.add_url_rule(acct_base + '/threads/', defaults={'thread_id': None},
                     view_func=thread_view, methods=['GET',])
 api_bp.add_url_rule('/threads/<int:thread_id>',
                     defaults={'account_id': None},
-                    view_func=thread_view,
-                    methods=['GET', 'DELETE'])
+                    view_func=thread_view, methods=['GET'])
+api_bp.add_url_rule('/threads/<int:thread_id>', view_func=thread_view,
+                    methods=['DELETE'])
 
 @api_bp.route(acct_nick_base + '/threads/<string:thread_id>',
               methods=['GET', 'DELETE'])
 def thread_by_name(nickname, thread_id):
-    account = Account.query.filter_by(nickname=nickname).first_or_404()
-    thread = Thread.query.filter_by(account_id=account.id).\
+    thread = Thread.query.filter(Account.nickname == nickname).\
         filter_by(thread_id=thread_id).first_or_404()
     return thread_view(None, thread.id)
-                       
