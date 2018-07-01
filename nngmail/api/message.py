ca098641d3d53d555cc88ce8c5c481118a4aebae
Ricardo E. Gonzalez <ricardog@ricardog.com>
2018-07-01 15:56:57 +0100

    Move get_ids() to utils so it can be used by other controllers

--- nngmail/api/message.py
+++ nngmail/api/message.py
@@ -8,24 +8,7 @@ from sqlalchemy.orm import undefer
 from nngmail import db, get_sync
 from nngmail.api import api_bp
 from nngmail.models import Account, Label, Message
-from nngmail.api.utils import acct_base, acct_nick_base
-
-def to_range(r):
-    """Convert a (low, high) tuple to a sequence of integers."""
-    if len(r) == 2:
-        return tuple(range(int(r[0]), int(r[1])+1))
-    return (int(r[0]), )
-
-def get_ids(arg):
-    """Convert a range string to a sequence of integers.
-
-A range string consists of comman separated ranges.  Each range is
-either a single number, or a low:high tuple.
-
-    """
-    return sum(map(lambda r: to_range(r),
-                   map(lambda s: s.split(':'),
-                       arg.split(','))), ())
+from nngmail.api.utils import acct_base, acct_nick_base, get_ids
 
 class MessageAPI(MethodView):
     def get(self, account_id, message_id):
--- nngmail/api/utils.py
+++ nngmail/api/utils.py
@@ -1,3 +1,20 @@
 
 acct_base = '/accounts/<int:account_id>'
 acct_nick_base = '/accounts/<string:nickname>'
+
+def to_range(r):
+    """Convert a (low, high) tuple to a sequence of integers."""
+    if len(r) == 2:
+        return tuple(range(int(r[0]), int(r[1])+1))
+    return (int(r[0]), )
+
+def get_ids(arg):
+    """Convert a range string to a sequence of integers.
+
+A range string consists of comman separated ranges.  Each range is
+either a single number, or a low:high tuple.
+
+    """
+    return sum(map(lambda r: to_range(r),
+                   map(lambda s: s.split(':'),
+                       arg.split(','))), ())
