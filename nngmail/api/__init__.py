d5fd4264a5addaaedd8cb37190c0d0bdba7fab62
Ricardo E. Gonzalez <ricardog@ricardog.com>
2021-03-08 16:21:56 +0000

    Add an endpoint to call pull synchronously
    I sometimes want to force a pull (for example when I wake-up my laptop)
    rather than wait for the next pull.  This change adds a new REST
    end-point that does a synchronous pull.  So, potentially I could use it
    a script that runs when my laptop wakes up.

--- nngmail/api/__init__.py
+++ nngmail/api/__init__.py
@@ -4,6 +4,7 @@ from flask import Blueprint, jsonify, request
 api_bp = Blueprint('api', __name__, url_prefix='/api/v1.0',
                    template_folder='templates')
 
+from nngmail import db, get_sync
 import nngmail.api.account
 import nngmail.api.mark
 import nngmail.api.message
@@ -13,7 +14,7 @@ import nngmail.api.thread
 
 
 ## Define a route for fetching contacts data
-from nngmail.models import Contact
+from nngmail.models import Account, Contact
 
 @api_bp.route('/contacts/', methods=['GET'])
 def contacts():
@@ -21,3 +22,9 @@ def contacts():
     return jsonify({'contacts': Contact.query.\
                     order_by(Contact.name.asc()).limit(limit).all()})
 
+@api_bp.route('/sync', methods=['POST'])
+def sync():
+    for account in Account.query.all():
+        if account.nickname != 'no.name':
+            get_sync(account).pull
+    return jsonify({'status': 'OK'})
